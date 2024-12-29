8bace8fe07da672a175598a4cafdd2dad7c95493
Prajun Trital <prajuncs@gmail.com>
2024-12-29 17:14:05 -0500

    Update Dropdown.tsx

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -31,7 +31,7 @@ const DropdownSearch: React.FC<DropdownProps> = ({
       <button
         id="dropdownSearchButton"
         onClick={() => setIsOpen(!isOpen)}
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="text-white w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         type="button"
       >
         {selectedOption || "Select a movie"}
@@ -55,7 +55,7 @@ const DropdownSearch: React.FC<DropdownProps> = ({
 
       {/* Dropdown menu */}
       {isOpen && (
-        <div className="z-10 absolute bg-white rounded-lg shadow w-60 dark:bg-gray-700 mt-2">
+        <div className="z-10 absolute bg-white rounded-lg shadow w-full dark:bg-gray-700 mt-2">
           <div className="p-3">
             <label htmlFor="input-group-search" className="sr-only">
               Search
